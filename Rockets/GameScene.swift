26ccae0d49bd504e098d20c66e13ddc7323eea25
David Stump <david@davidstump.net>
2014-12-04 17:35:50 -0500

    Make it so

--- Rockets/GameScene.swift
+++ Rockets/GameScene.swift
@@ -7,39 +7,106 @@
 //
 
 import SpriteKit
+import Foundation
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+  let socket = Phoenix.Socket(endPoint: "ws://localhost:4000/ws")
+  var topic: String? = "lobby"
+  var rocket = SKSpriteNode()
+  
+  override func didMoveToView(view: SKView) {
+    channelListeners()
+  }
+  
+  func channelListeners() {
+    // Join the socket and establish handlers for users entering and submitting messages
+    socket.join("rooms", topic: topic!, message: Phoenix.Message(subject: "status", body: "joining")) { channel in
+      let chan = channel as Phoenix.Channel
+      
+      chan.on("join") { message in
+        println("create my rocket")
+        let name = message.valueFor("id")
+        self.createMyRocket(name)
+      }
+      
+      chan.on("player:move") { message in
+        let name: String = message.valueFor("id")
+        let move: String = message.valueFor("move")
+        println("move player")
+        self.moveRocket(name, move: move)
+      }
+      
+      chan.on("user:entered") { message in
+        println("create another rocket")
+        let name: String = message.valueFor("id")
+        self.createRocket(name)
+      }
+      
+      chan.on("user:leave") { message in
+        println("destroy rocket")
+        self.destroyRocket()  
+      }
     }
+  }
+  
+  func moveRocket(name: String, move: String) {
+    var guest: SKSpriteNode = childNodeWithName(name) as SKSpriteNode
+    let point = convertMoveToPoint(move)
+    let fly = SKAction.moveTo(point, duration: 0.5)
+    guest.runAction(fly)
+  }
+  
+  func createMyRocket(name: String) {
+    rocket = SKSpriteNode(imageNamed: "rocket")
+    rocket.name = name
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+    rocket.xScale = 0.1
+    rocket.yScale = 0.1
+    
+    rocket.position = CGPointMake(500, 400)
+    
+    self.addChild(rocket)
+  }
+  
+  func createRocket(name: String) {
+    let guest = SKSpriteNode(imageNamed: "rocket")
+    guest.name = name
+    
+    guest.xScale = 0.1
+    guest.yScale = 0.1
+    
+    guest.position = CGPointMake(500, 400)
+    
+    self.addChild(guest)
+  }
+  
+  func destroyRocket() {
+  }
+  
+  override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+    /* Called when a touch begins */
+    for touch in touches {
+      let clickPoint = touch.locationInNode(self)
+      let fly = SKAction.moveTo(clickPoint, duration: 0.5)
+      rocket.runAction(fly)
+      let message = Phoenix.Message(message: ["user": "iDavid", "move":"\(clickPoint.x):\(clickPoint.y)"])
+      let payload = Phoenix.Payload(channel: "rooms", topic: topic!, event: "player:move", message: message)
+      socket.send(payload)
     }
+  }
    
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-    }
+  override func update(currentTime: CFTimeInterval) {
+    /* Called before each frame is rendered */
+  }
+  
+  func convertMoveToPoint(move: String) -> CGPoint {
+    let position: Array = move.componentsSeparatedByString(":")
+    let (positionX: Double, positionY: Double) = (doubleFromString(position[0])!, doubleFromString(position[1])!)
+    let point = CGPoint(x: positionX, y: positionY)
+    return point
+  }
+  
+  func doubleFromString(string: String) -> Double? {
+    return NSNumberFormatter().numberFromString(string)?.doubleValue
+  }
 }
