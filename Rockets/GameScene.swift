06f08c73eb2df0eaf82fd3a91b3a0e14318abbb3
David Stump <david@davidstump.net>
2014-12-05 23:39:25 -0500

    Update profiles

--- Rockets/GameScene.swift
+++ Rockets/GameScene.swift
@@ -10,7 +10,7 @@ import SpriteKit
 import Foundation
 
 class GameScene: SKScene {
-  let socket = Phoenix.Socket(endPoint: "ws://localhost:4000/ws")
+  let socket = Phoenix.Socket(endPoint: "ws://192.168.1.68:4000/ws")
   var topic: String? = "lobby"
   var rocket = SKSpriteNode()
   
@@ -24,7 +24,6 @@ class GameScene: SKScene {
       let chan = channel as Phoenix.Channel
       
       chan.on("join") { message in
-        println("create my rocket")
         let name = message.valueFor("id")
         self.createMyRocket(name)
       }
@@ -32,28 +31,35 @@ class GameScene: SKScene {
       chan.on("player:move") { message in
         let name: String = message.valueFor("id")
         let move: String = message.valueFor("move")
-        println("move player")
         self.moveRocket(name, move: move)
       }
       
       chan.on("user:entered") { message in
-        println("create another rocket")
         let name: String = message.valueFor("id")
         self.createRocket(name)
       }
       
       chan.on("user:leave") { message in
-        println("destroy rocket")
-        self.destroyRocket()  
+        let name = message.valueFor("id")
+        self.destroyRocket(name)
       }
     }
   }
   
-  func moveRocket(name: String, move: String) {
-    var guest: SKSpriteNode = childNodeWithName(name) as SKSpriteNode
-    let point = convertMoveToPoint(move)
+  func fly(sprite: SKSpriteNode, point: CGPoint) {
     let fly = SKAction.moveTo(point, duration: 0.5)
-    guest.runAction(fly)
+    rotateTowardTouch(point)
+    sprite.runAction(fly)
+  }
+  
+  func moveRocket(name: String, move: String) {
+    if var guest: SKSpriteNode = childNodeWithName(name) as? SKSpriteNode {
+      let point = convertMoveToPoint(move)
+      fly(guest, point: point)
+    } else {
+      createRocket(name)
+      moveRocket(name, move: move)
+    }
   }
   
   func createMyRocket(name: String) {
@@ -80,19 +86,37 @@ class GameScene: SKScene {
     self.addChild(guest)
   }
   
-  func destroyRocket() {
+  func destroyRocket(name: String) {
+    if var guest: SKSpriteNode = childNodeWithName(name) as? SKSpriteNode {
+        guest.removeFromParent()
+    }
   }
   
   override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
     /* Called when a touch begins */
     for touch in touches {
-      let clickPoint = touch.locationInNode(self)
-      let fly = SKAction.moveTo(clickPoint, duration: 0.5)
-      rocket.runAction(fly)
-      let message = Phoenix.Message(message: ["user": "iDavid", "move":"\(clickPoint.x):\(clickPoint.y)"])
-      let payload = Phoenix.Payload(channel: "rooms", topic: topic!, event: "player:move", message: message)
-      socket.send(payload)
+      let point = touch.locationInNode(self)
+      fly(rocket, point: point)
+      broadcastMove(rocket, point: point)
     }
+  }
+  
+  func broadcastMove(ship: SKSpriteNode, point: CGPoint) {
+    let message = Phoenix.Message(message: ["move":"\(point.x):\(point.y)", "id": "\(ship.name)"])
+    let payload = Phoenix.Payload(channel: "rooms", topic: topic!, event: "player:move", message: message)
+    socket.send(payload)
+  }
+  
+  func rotateTowardTouch(point: CGPoint) {
+    let deltaX: Float = Float(point.x - rocket.position.x)
+    let deltaY: Float = Float(point.y - rocket.position.y)
+    let angle = atan2f(deltaY, deltaX)
+    let radians = CGFloat(angle - degreesToRadians(90.0))
+    rocket.zRotation = radians
+  }
+  
+  func degreesToRadians(angle: Float) -> Float {
+    return angle * 0.01745329252
   }
    
   override func update(currentTime: CFTimeInterval) {
